2ad2aac2c692ca1faae7f34978355375b6a57f51
Student <josse.coen@gmail.com>
2015-11-05 12:04:36 +0100

    Added Apache Avro files to chat-app

---
+++ chat-app/src/client/Client.java
@@ -0,0 +1,19 @@
+package client;
+
+/*
+ * - automatische registratie bij de server
+ * - lijst opvragen van alle online gebruikers
+ * - gebruiker kan publieke chatroom joinen
+ * - requests voor prive gesprekken sturen, accepteren, weigeren
+ *   (in elke situatie: publieke chatroom, prive gesprek)
+ * - video streaming
+ * - maximaal 1 conversatie tegelijk
+ */
+
+public class Client {
+	public static void main(String[] argv) {
+		
+	}
+}
+
+// Runtime.getRuntime().addShutdownHook
