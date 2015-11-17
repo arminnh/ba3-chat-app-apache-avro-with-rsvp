4d8a1623b1dab1d80b12c18230864128ba890219
Student <josse.coen@gmail.com>
2015-11-17 16:59:51 +0100

    Began to define RSVP objects

--- click-rsvp/click-2.0.1/elements/local/RSVPElement.cc
+++ click-rsvp/click-2.0.1/elements/local/RSVPElement.cc
@@ -26,7 +26,13 @@ Packet* RSVPElement::pull(int){
 
 int RSVPElement::handle(const String &conf, Element *e, void * thunk, ErrorHandler *errh) {
   RSVPElement * me = (RSVPElement *) e;
-  if(cp_va_kparse(conf, me, errh, cpEnd) < 0) return -1;
+  
+  int i;
+  
+  if(cp_va_kparse(conf, me, errh, "INTEGER", cpkM + cpkP, cpInteger, &i, cpEnd) < 0) return -1;
+  
+  click_chatter("received integer: %d", (int) i);
+  
   //me->doSomething
   return 666;
 }
@@ -37,8 +43,8 @@ String RSVPElement::handle2(Element *e, void * thunk) {
 }
 
 void RSVPElement::add_handlers() {
-  add_write_handler("a_handle", &handle, (void *)0);
-  add_read_handler("a_handle2", &handle2, (void *)0);
+  add_write_handler("a", &handle, (void *)0);
+  add_read_handler("b", &handle2, (void *)0);
 }
 
 CLICK_ENDDECLS
