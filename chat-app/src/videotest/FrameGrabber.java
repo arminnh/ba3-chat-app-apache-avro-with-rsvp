93424d72a9d6bf5547394dacc2f5e4cc6734353e
Armin <halilovic.armin@hotmail.com>
2015-12-19 20:57:52 +0100

    Improved video framerate and made it resizable. Removed output file.

--- chat-app/src/videotest/FrameGrabber.java
+++ chat-app/src/videotest/FrameGrabber.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 
@@ -20,83 +21,58 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /**
- * Using {@link IMediaReader}, takes a media container, finds the first video 
- * stream, decodes that stream, and then writes video frames out to a PNG image 
- * file, based on the video presentation timestamps.
- *
- * @author aclarke
- * @author trebor
+ * Using IMediaReader, takes a media container, finds the first video  stream, decodes that stream, 
+ * and then writes video frames out to a PNG image file, based on the video presentation timestamps.
  */
 public class FrameGrabber extends MediaListenerAdapter {
-
-    private int CAPTURED_FRAMES = 0;        // count of the number of frames captures
-    private int MAX_CAPTURED_FRAMES = 10000;    // maximum number of frames to capture
+    private int CAPTURED_FRAMES = 0; // count of the number of frames captures
     
     // The number of seconds between frames.
-    public static final double SECONDS_BETWEEN_FRAMES = 1/25;
+    public static double SECONDS_BETWEEN_FRAMES = 1/25;
 
     // The number of micro-seconds between frames.
     public static long MICRO_SECONDS_BETWEEN_FRAMES = (long) (Global.DEFAULT_PTS_PER_SECOND * SECONDS_BETWEEN_FRAMES);
 
-    // Time of last frame write.
+    // Time of last frame write. NO_PTS means no time stamp is set
     private static long mLastPtsWrite = Global.NO_PTS;
     
     // The video stream index, used to ensure we display frames from one and only one video stream from the media container.
     private int mVideoStreamIndex = -1;
+    private boolean isSet = false;
 
-    private File input;
-    private File output;
-    
     private static final Logger logger = Logger.getLogger(FrameGrabber.class.getName());
 
+    private File input;
     private JFrame frame;
     private Graphics g;
-    
     private IMediaReader reader = null;
     
-    private boolean isSet = false;
-    //--------------------------------------------------------------------------
+    //====================================================================================================================
     
-    /** 
-     * FrameGrabber constructor.
-     * @param Input Absolute path to input file
-     * @param Output Absolute path to output file
-     */
-    public FrameGrabber(File Input, File Output, JFrame frame) {
-        input = Input;
-        output = Output;
+    public FrameGrabber(File Input, JFrame frame) {
+    	this.input = Input;
         
         this.frame = frame;
         this.g = frame.getGraphics();
         
         // create a media reader for processing video
-        reader = ToolFactory.makeReader(Input.getAbsolutePath());
-//        MAX_CAPTURED_FRAMES = reader.
+        this.reader = ToolFactory.makeReader(Input.getAbsolutePath());
 
         // stipulate that we want BufferedImages created in BGR 24bit color space
-        reader.setBufferedImageTypeToGenerate(BufferedImage.TYPE_3BYTE_BGR);
+        this.reader.setBufferedImageTypeToGenerate(BufferedImage.TYPE_3BYTE_BGR);
 
-        // note that DecodeAndCaptureFrames is derived from MediaReader.ListenerAdapter and thus may be added as a listener
-        // to the MediaReader. DecodeAndCaptureFrames implements onVideoPicture().
-        reader.addListener(this);
-
-        System.out.println("frames to capture: " + MAX_CAPTURED_FRAMES);
-        System.out.println("Seconds between frames: " + SECONDS_BETWEEN_FRAMES);
-        System.out.println("Microseconds between frames: " + MICRO_SECONDS_BETWEEN_FRAMES);
-        System.out.println("Time of last frame write: " + mLastPtsWrite);
-        System.out.println("DEFAULT_PTS_PER_SECOND: " + Global.DEFAULT_PTS_PER_SECOND);
-        System.out.println("Video stream index: " + mVideoStreamIndex);
+        // note that DecodeAndCaptureFrames is derived from MediaReader.ListenerAdapter 
+        // and thus may be added as a listener to the MediaReader. DecodeAndCaptureFrames implements onVideoPicture().
+        this.reader.addListener(this);
         
-        // read out the contents of the media file, note that nothing else happens here.  
-        // action happens in the onVideoPicture() method which is called when complete 
-        // video pictures are extracted from the media source
-//        while (reader.readPacket() == null && CAPTURED_FRAMES < MAX_CAPTURED_FRAMES) {
-        while (reader.readPacket() == null && CAPTURED_FRAMES < MAX_CAPTURED_FRAMES) {
-            do {
-            } while (false);
+        // read out the contents of the media file, note that nothing else happens here. 
+        // action happens in the onVideoPicture() method which is called when complete video pictures are extracted from the media source
+        while (this.reader.readPacket() == null) {
+            do { } while (false);
         }
 
-        System.out.println("frames to capture: " + MAX_CAPTURED_FRAMES);
+        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+        
         System.out.println("Seconds between frames: " + SECONDS_BETWEEN_FRAMES);
         System.out.println("Microseconds between frames: " + MICRO_SECONDS_BETWEEN_FRAMES);
         System.out.println("Time of last frame write: " + mLastPtsWrite);
@@ -104,66 +80,63 @@ public class FrameGrabber extends MediaListenerAdapter {
         System.out.println("Video stream index: " + mVideoStreamIndex);
     }
     
-    //--------------------------------------------------------------------------
 
     /**
-     * Called after a video frame has been decoded from a media stream.
-     * Optionally a BufferedImage version of the frame may be passed
-     * if the calling {@link IMediaReader} instance was configured to
-     * create BufferedImages. This method blocks, so return quickly.
+     * Called after a video frame has been decoded from a media stream. Optionally a BufferedImage version of the frame may be passed
+     * if the calling IMediaReader instance was configured to create BufferedImages. This method blocks, so return quickly.
      */
     public void onVideoPicture(IVideoPictureEvent event) {
     	System.out.println("Frame nr: " + CAPTURED_FRAMES);
+    	
+    	try {
+    		Thread.sleep((long) reader.getContainer().getStream(mVideoStreamIndex).getFrameRate().getValue());
+    	} catch (Exception e) {
+    		//e.printStackTrace();
+    	}
+    	
         try {
             // if the stream index does not match the selected stream index, then have a closer look
             if (event.getStreamIndex() != mVideoStreamIndex) {
                 // if the selected video stream id is not yet set, go ahead an select this lucky video stream
                 if (-1 == mVideoStreamIndex) {
                     mVideoStreamIndex = event.getStreamIndex();
-                } // otherwise return, no need to show frames from this video stream
+                } 
+                // otherwise return, no need to show frames from this video stream
                 else {
                     return;
                 }
             }
             
-            if (!isSet) {
+            /*if (!isSet) {
                 double fps = reader.getContainer().getStream(mVideoStreamIndex).getFrameRate().getDouble();
                 System.out.println(MICRO_SECONDS_BETWEEN_FRAMES);
                 MICRO_SECONDS_BETWEEN_FRAMES = (long) (Global.DEFAULT_PTS_PER_SECOND * 1/fps);
                 System.out.println(MICRO_SECONDS_BETWEEN_FRAMES);
                 isSet = true;
-            }
-            
+            }*/
+
             // if uninitialized, backdate mLastPtsWrite so we get the very first frame
             if (mLastPtsWrite == Global.NO_PTS) {
                 mLastPtsWrite = event.getTimeStamp() - MICRO_SECONDS_BETWEEN_FRAMES;
             }
-//            while () {
-//            	Thread.sleep();
-//            }
+
             // if it's time to write the next frame
             if (event.getTimeStamp() - mLastPtsWrite >= MICRO_SECONDS_BETWEEN_FRAMES) {
-                // write out PNG
-        		RenderedImage im = event.getImage();
-//                ImageIO.write(im, "png", output);
-//                ImageIO.write(event.getImage(), "png", img);
-                
-                // indicate file written
-//                double seconds = ((double) event.getTimeStamp()) / Global.DEFAULT_PTS_PER_SECOND;
-                
-                // update last write time
-                mLastPtsWrite += MICRO_SECONDS_BETWEEN_FRAMES;
-                CAPTURED_FRAMES++;
-//                System.out.println("Wrote frame to file");
-                
-                BufferedImage img = (BufferedImage) im;
-				g.drawImage(img, 0, 0, 800, 600, null);
+            	//write out PNG
+            	//ImageIO.write(event.getImage(), "png", img);
+
+            	// update last write time
+            	mLastPtsWrite += MICRO_SECONDS_BETWEEN_FRAMES;
+            	CAPTURED_FRAMES++;
+
+            	BufferedImage img = event.getImage();
+            	g.drawImage(img, 0, 0, frame.getWidth(), frame.getHeight(), null);
             }
             
         } catch (Exception ex) {
             //String stack = ExceptionUtils.getStackTrace(ex);
         	String stack = ex.getLocalizedMessage();
-            logger.log(Level.SEVERE, "Could not write poster image for video file {0}\n\n{1}", new Object[]{input.getAbsolutePath(),stack});
+            System.err.println(Level.SEVERE.toString() + "Could not write poster image for video file {0}\n\n{1} " + new Object[]{input.getAbsolutePath(),stack});
         }
     }
-} // end class
+}
--- chat-app/src/videotest/MainTest.java
+++ chat-app/src/videotest/MainTest.java
@@ -2,27 +2,27 @@ package videotest;
 
 import java.io.File;
 import java.io.IOException;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainTest extends JPanel {
 
-	public BufferedImage img = null;
-	public Graphics g = null;
-	public File a = null, b = null;
+	public File input = null;
+	public File output = null;
+	private static final long serialVersionUID = 1L;
 
+    //====================================================================================================================
+	
 	public static void main(String[] args) throws IOException {
 		System.out.println("Working Directory = " + System.getProperty("user.dir"));
 		MainTest test = new MainTest();
 
-//		test.a = new File("SampleVideo_1080x720_30mb.flv");
-//		test.a = new File("SampleVideo_1080x720_50mb.mp4");
-		test.a = new File("SampleVideo_1080x720_20mb.mkv");
-//		test.a = new File("small.flv");
-		test.b = new File("myFile.png");
+//		test.input = new File("SampleVideo_1080x720_30mb.flv");
+//		test.input = new File("SampleVideo_1080x720_50mb.mp4");
+		test.input = new File("SampleVideo_1080x720_20mb.mkv");
+//		test.input = new File("small.flv");
+		test.output = new File("myFile.png");
 
 		JFrame frame = new JFrame();
 		frame.getContentPane().add(test);
@@ -30,17 +30,6 @@ public class MainTest extends JPanel {
 		frame.setSize(800, 600);
 		frame.setVisible(true);
 		
-		new FrameGrabber(test.a, test.b, frame);
+		new FrameGrabber(test.input, frame);
 	}
-
-	/*public void paint(Graphics g)  {
-		System.out.println("paint");
-		try {
-			img = ImageIO.read(this.b);
-		    g.drawString("www.java2s.com", 20,20);
-			g.drawImage(img, 0, 0, 800, 600, this);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}*/
 }
