9a471f53c8127ab44ad4176ebc4a94354c3b5483
student <halilovic.armin@hotmail.com>
2015-12-23 17:56:21 +0100

    Trying to improve video performance

--- chat-app/src/video/VideoSender.java
+++ chat-app/src/video/VideoSender.java
@@ -1,64 +1,47 @@
 package video;
 
-import javax.imageio.ImageIO;
-
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.nio.ByteBuffer;
+import javax.swing.JFrame;
 import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
-
-import client.AppClientInterface;
+import javax.imageio.ImageIO;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.ByteArrayOutputStream;
 
 import com.xuggle.mediatool.IMediaReader;
 import com.xuggle.mediatool.MediaListenerAdapter;
 import com.xuggle.mediatool.ToolFactory;
 import com.xuggle.mediatool.event.IVideoPictureEvent;
 import com.xuggle.xuggler.Global;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import com.xuggle.xuggler.ICodec;
+import com.xuggle.xuggler.IContainer;
+import com.xuggle.xuggler.IStream;
 
-import javax.swing.JFrame;
-
-import org.apache.avro.AvroRemoteException;
+import client.AppClientInterface;
 
 /**
  * Using IMediaReader, takes a media container, finds the first video  stream, decodes that stream, 
  * and then writes video frames out to a PNG image file, based on the video presentation timestamps.
  */
 public class VideoSender extends MediaListenerAdapter implements Runnable {
-    private int CAPTURED_FRAMES = 0; // count of the number of frames captures
-    
-    // The number of seconds between frames.
-    public static double SECONDS_BETWEEN_FRAMES = 1/25;
-
-    // The number of micro-seconds between frames.
-    public static long MICRO_SECONDS_BETWEEN_FRAMES = (long) (Global.DEFAULT_PTS_PER_SECOND * SECONDS_BETWEEN_FRAMES);
-
-    // Time of last frame write. NO_PTS means no time stamp is set
-    private static long mLastPtsWrite = Global.NO_PTS;
+	private long MICRO_SECONDS_PER_FRAME = -1;
     
     // The video stream index, used to ensure we display frames from one and only one video stream from the media container.
-    private int mVideoStreamIndex = -1;
-    private boolean isSet = false;
+    private int videoStreamIndex = -1;
 
-    private static final Logger logger = Logger.getLogger(VideoSender.class.getName());
-
-    private File input;
     private JFrame frame;
     private Graphics g;
     private AppClientInterface proxy;
     private IMediaReader reader = null;
+    private Queue<BufferedImage> imgBuffer = new LinkedList<BufferedImage>();
+    private boolean videoDone;
     
     //====================================================================================================================
 
     public VideoSender(File input, JFrame frame, AppClientInterface proxy) {
-    	this.input = input;
         this.frame = frame;
         this.proxy = proxy;
         this.g = this.frame.getGraphics();
@@ -72,6 +55,28 @@ public class VideoSender extends MediaListenerAdapter implements Runnable {
         // note that DecodeAndCaptureFrames is derived from MediaReader.ListenerAdapter 
         // and thus may be added as a listener to the MediaReader. DecodeAndCaptureFrames implements onVideoPicture().
         this.reader.addListener(this);
+        
+        IContainer container = IContainer.make();
+        if (container.open(input.getAbsolutePath(), IContainer.Type.READ, null) < 0)
+        	throw new IllegalArgumentException("could not open file: " + input);
+        
+		for (int i = 0; i < container.getNumStreams(); i++) {
+			// Find the stream object
+			System.out.println(i);
+			IStream stream = container.getStream(i);
+			
+			// Get the pre-configured decoder that can decode this stream;
+			if (stream.getStreamCoder().getCodecType() == ICodec.Type.CODEC_TYPE_VIDEO) {
+				videoStreamIndex = i;
+				break;
+			}
+		}
+		if (videoStreamIndex == -1)
+			throw new RuntimeException("could not find video stream in container: " + input);
+        
+        double fps = container.getStream(videoStreamIndex).getFrameRate().getDouble();
+        MICRO_SECONDS_PER_FRAME = (long) (1000000.0 / fps);
+        System.out.println("FPS: " + fps + ", microseconds per frame: " + MICRO_SECONDS_PER_FRAME);
     }
     
     public void send() {
@@ -80,7 +85,7 @@ public class VideoSender extends MediaListenerAdapter implements Runnable {
         while (this.reader.readPacket() == null && frame.isVisible()) {
             do { } while (false);
         }
-
+        videoDone = true;
         /*System.out.println("Seconds between frames: " + SECONDS_BETWEEN_FRAMES);
         System.out.println("Microseconds between frames: " + MICRO_SECONDS_BETWEEN_FRAMES);
         System.out.println("Time of last frame write: " + mLastPtsWrite);
@@ -94,63 +99,32 @@ public class VideoSender extends MediaListenerAdapter implements Runnable {
      * if the calling IMediaReader instance was configured to create BufferedImages. This method blocks, so return quickly.
      */
     public void onVideoPicture(IVideoPictureEvent event) {
-    	//System.out.println("Frame nr: " + CAPTURED_FRAMES);
+        if (event.getStreamIndex() != videoStreamIndex) {
+            return;
+        }
+    	imgBuffer.add(event.getImage());
+    }
+    
+    public void sendVideo() {
+    	long lastFrameWrite = System.nanoTime()/1000 - MICRO_SECONDS_PER_FRAME;
+    	long time_passed = 0;
     	
-    	/*try {
-    		Thread.sleep((long) reader.getContainer().getStream(mVideoStreamIndex).getFrameRate().getValue());
-    	} catch (Exception e) {
-    		//e.printStackTrace();
-    	}*/
-    	
-        try {
-            // if the stream index does not match the selected stream index, then have a closer look
-            if (event.getStreamIndex() != mVideoStreamIndex) {
-                // if the selected video stream id is not yet set, go ahead an select this lucky video stream
-                if (-1 == mVideoStreamIndex) {
-                    mVideoStreamIndex = event.getStreamIndex();
-                } 
-                // otherwise return, no need to show frames from this video stream
-                else {
-                    return;
-                }
-            }
-            
-            /*if (!isSet) {
-                double fps = reader.getContainer().getStream(mVideoStreamIndex).getFrameRate().getDouble();
-                System.out.println(MICRO_SECONDS_BETWEEN_FRAMES);
-                MICRO_SECONDS_BETWEEN_FRAMES = (long) (Global.DEFAULT_PTS_PER_SECOND * 1/fps);
-                System.out.println(MICRO_SECONDS_BETWEEN_FRAMES);
-                isSet = true;
-            }*/
+    	try {
+    		while (!videoDone || !imgBuffer.isEmpty()) {
+    			time_passed = System.nanoTime()/1000 - lastFrameWrite;
+    			if (!imgBuffer.isEmpty() && time_passed >= MICRO_SECONDS_PER_FRAME) {
+    				lastFrameWrite = System.nanoTime()/1000;
+    				BufferedImage img = imgBuffer.remove();
+    				g.drawImage(img, 0, 0, frame.getWidth(), frame.getHeight(), null);
 
-            // if uninitialized, backdate mLastPtsWrite so we get the very first frame
-            if (mLastPtsWrite == Global.NO_PTS) {
-                mLastPtsWrite = event.getTimeStamp() - MICRO_SECONDS_BETWEEN_FRAMES;
-            }
-
-            // if it's time to write the next frame
-            if (event.getTimeStamp() - mLastPtsWrite >= MICRO_SECONDS_BETWEEN_FRAMES) {
-            	// update last write time
-            	mLastPtsWrite += MICRO_SECONDS_BETWEEN_FRAMES;
-            	CAPTURED_FRAMES++;
-
-            	BufferedImage img = event.getImage();
-            	g.drawImage(img, 0, 0, frame.getWidth(), frame.getHeight(), null);
-            	
-        		try {
-        			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                	ImageIO.write(img,  "jpg",  bos);
-            		proxy.receiveImage(ByteBuffer.wrap(bos.toByteArray()));
-        		} catch (IOException e) {
-        			// other user has disconnected, stop sending video
-        			frame.setVisible(false);
-        		} 
-            }
-        } catch (Exception e) {
-        	e.printStackTrace();
-            System.err.println(Level.SEVERE.toString() 
-            		+ "Could not write poster image for video file {0}\n\n{1} " 
-            		+ new Object[] {input.getAbsolutePath(), e.getLocalizedMessage()});
+    				ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    				ImageIO.write(img,  "jpg",  bos);
+    				proxy.receiveImage(ByteBuffer.wrap(bos.toByteArray()));
+    			}
+    		}
+    	} catch (IOException e) {
+			// other user has disconnected, stop sending video
+			frame.setVisible(false);
         }
     }
 
